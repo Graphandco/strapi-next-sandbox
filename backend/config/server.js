710cac927080774c11977b406a7f122244a23c40
Régis Daum <rdaum@john-steel.com>
2025-05-09 19:22:51 +0200

    Removed server config

--- backend/config/server.js
+++ backend/config/server.js
@@ -4,15 +4,6 @@ module.exports = ({ env }) => ({
 	app: {
 		keys: env.array("APP_KEYS"),
 	},
-	url: env("APP_URL", "http://localhost:1337"), // Si tu utilises un autre port/URL pour Strapi
-	admin: {
-		auth: {
-			secret: env("ADMIN_JWT_SECRET"),
-		},
-	},
-	server: {
-		allowedHosts: ["strapiback.graphandco.com"], // Ajouter ici ton domaine
-	},
 	webhooks: {
 		populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
 	},
