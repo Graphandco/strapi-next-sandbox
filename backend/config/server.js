b947f17b93f3630453479e10ee9ec9fc471eb3c4
Régis Daum <rdaum@john-steel.com>
2025-05-10 11:57:41 +0200

    Removed allowedHosts

--- backend/config/server.js
+++ backend/config/server.js
@@ -4,13 +4,6 @@ module.exports = ({ env }) => ({
 	app: {
 		keys: env.array("APP_KEYS"),
 	},
-	server: {
-		allowedHosts: [
-			"strapiback.graphandco.com", // Ton domaine spécifique
-			"localhost", // Permet aussi les requêtes locales
-			"0.0.0.0", // Permet toutes les adresses
-		],
-	},
 	webhooks: {
 		populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
 	},
